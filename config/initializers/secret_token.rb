f99822e66763b524ba279b5364e55bf662423b45
adam.t.wolf@gmail.com <adam@ubuntu.(none)>
2012-02-25 11:39:10 -0800

    Adding CCC website to repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoastalCommunicationConsultants::Application.config.secret_token = 'd5c62587a4f02373da8986fe64c07d35048649d794586c34ff6835b349e7f03444f60bba2dd620ad9c44fcd2e10aacc5ec47613ea9decfd937e0d5f278137fa4'
