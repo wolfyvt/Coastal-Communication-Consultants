57a36038510966fdde0023c6a914904796f5e2ab
Adam Wolf <adam.t.wolf@gmail.com>
2012-04-10 15:21:49 -0700

    removed the last migration - it failed.  Scaffold.css is also removed which cleans up some of the appearance.  Maybe some other small adjustments.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120321023547) do
+ActiveRecord::Schema.define(:version => 20120321023951) do
 
   create_table "faqs", :force => true do |t|
     t.string   "question"
     t.string   "answer"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "order_index"
   end
 
   create_table "information_requests", :force => true do |t|
