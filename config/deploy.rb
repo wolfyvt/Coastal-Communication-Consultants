5b05bd77128e64dcca90f3cbab951efef708f9cd
Lee Stubbs <lee.stubbs@gmail.com>
2013-02-19 20:29:22 -0500

    Updated deployment files

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,17 +2,19 @@
 # For production pre compilation of assets use the following from the root project directory (may need to run as www-data for permissions):
 # bundle exec rake assets:precompile
 
-# Use agent forwarding to use local github keys
-ssh_options[:forward_agent] = true
+desc "Starting deployment..."
 
 # This is often needed to ask for passwords...
 default_run_options[:pty] = true
+
+# Use agent forwarding to use local github keys
+ssh_options[:forward_agent] = true
 
 # Set Rails environment test, development, production
 set :rails_env, :production
 
 # Set domain user name (not sure how to ask during deployment)
-set :user, "lee"
+#set :user, "lee"
 #set :user, "adam"
 
 # Set Domain and Repository Configurations
@@ -34,6 +36,18 @@ role :db,  domain, :primary => true # This is where Rails migrations will run
 # these http://github.com/rails/irs_process_scripts
 
 namespace :deploy do
+  
+   desc "Backing up production database."
+   t=Time.now
+   run "cp /var/www/ccc/db/production.sqlite3 /var/www/ccc/db/backup/#{t.year}-#{t.month}-#{t.day}_#{t.hour}:#{t.min}:#{t.sec}_db.sqlite3"
+  
+   desc "Create symbolic link to production database."
+   run "ln -s /var/www/ccc/db/production.sqlite3 production.sqlite3"
+   
+   desc "Updating owner and permissions."
+   run "chown www-data:www-data -R /var/www/ccc"
+   run "chmod 755 -R /var/www/ccc"
+  
    desc "Cause Passenger to restart applicaton."
    task :restart do
      run "touch #{deploy_to}current/tmp/restart.txt"
@@ -48,3 +62,4 @@ set :deploy_via, :remote_cache
 
 # Turn on verbose mode for software configuration management
 set :scm_verbose, true
+
