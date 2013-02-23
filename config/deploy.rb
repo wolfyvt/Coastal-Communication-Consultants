1e2cf1de5d327a6353212b0f6173fe63f120669c
Lee Stubbs <lee.stubbs@gmail.com>
2013-02-23 14:50:32 -0500

    Major updates to the deployment script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,3 @@
-# ALS deployment notes:
-# For production pre compilation of assets use the following from the root project directory (may need to run as www-data for permissions):
-# bundle exec rake assets:precompile
-
 # This is often needed to ask for passwords...
 default_run_options[:pty] = true
 
@@ -11,10 +7,9 @@ ssh_options[:forward_agent] = true
 # Set Rails environment test, development, production
 set :rails_env, :production
 
-# Set domain user name (not sure how to ask during deployment)
+# Set domain user name
+# Must add deployment .pub keys to /var/www/.ssh authorized_keys
 set :user, "www-data"
-#set :user, "lee"
-#set :user, "adam"
 
 # Set Domain and Repository Configurations
 set :domain, "coastalcommunicationconsultants.com"
@@ -34,15 +29,27 @@ role :db,  domain, :primary => true # This is where Rails migrations will run
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
+
+
 namespace :deploy do
   
-  desc "Backing up production database."
-  t=Time.now
-  run "cp #{deploy_to}db/production.sqlite3 #{deploy_to}db/backup/#{t.year}-#{t.month}-#{t.day}_#{t.hour}:#{t.min}:#{t.sec}_db.sqlite3"
+  desc "Backs up production db."
+  task :backup_sqlite3_db do
+    puts "Backing up production database."
+    t=Time.now
+    run "cp #{deploy_to}db/production.sqlite3 #{deploy_to}db/backup/#{t.year}-#{t.month}-#{t.day}_#{t.hour}:#{t.min}:#{t.sec}_db.sqlite3"
+  end
   
-  desc "Create symbolic link to production database."
-  task :after_symlink
+  desc "Creates symbolic link for production db."
+  task :create_db_symlink do
+    puts "Create symbolic link for production sqlite3 database."
     run "ln -s #{deploy_to}db/production.sqlite3 #{deploy_to}current/db/production.sqlite3"
+  end
+  
+  desc "Precompiles all assets for project."
+  task :precompile_assets do
+    puts "Pre-compiling assets for #{deploy_to}current..."
+    run "cd #{deploy_to}current; bundle exec rake assets:precompile"
   end
         
    desc "Cause Passenger to restart applicaton."
@@ -59,4 +66,9 @@ set :deploy_via, :remote_cache
 
 # Turn on verbose mode for software configuration management
 set :scm_verbose, true
+
+# Setup task execution steps
+before "deploy:update", "deploy:backup_sqlite3_db"
+after  "deploy:create_symlink", "deploy:create_db_symlink"
+before "deploy:restart", "deploy:precompile_assets"
 
